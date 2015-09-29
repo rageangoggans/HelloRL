4b7656400318ba4bf89b7df0ece005fe5847590f
Your Name <you@example.com>
2015-09-28 22:20:31 -0400

    Hello Ragean

---
+++ ConsoleApplication37/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = (Console.ReadLine());
+            string result = Hello(name);
+            Console.WriteLine(result);
+        }
+
+        static string Hello(string name)
+        {
+            string Hello = "Hello " +" "+ name;
+            return Hello;
+        }
+
+    }
+
+
+}
